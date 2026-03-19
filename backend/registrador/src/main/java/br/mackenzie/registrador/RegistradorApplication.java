2f127b1779147a8b09f5efa56832a8483818a50a
ArthurEdu05 <arthuredu2005@gmail.com>
2026-03-19 16:30:22 -0300

    initial project setup

---
+++ backend/registrador/src/main/java/br/mackenzie/registrador/RegistradorApplication.java
@@ -0,0 +1,13 @@
+package br.mackenzie.registrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistradorApplication.class, args);
+	}
+
+}
---
+++ backend/registrador/src/test/java/br/mackenzie/registrador/RegistradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.registrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
