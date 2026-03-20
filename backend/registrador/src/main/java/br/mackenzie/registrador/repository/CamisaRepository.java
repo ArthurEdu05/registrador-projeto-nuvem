ae8fadff121a31281cab32b362d900abd7a58b0a
ArthurEdu05 <arthuredu2005@gmail.com>
2026-03-20 10:48:00 -0300

    feat: Adjust CamisaRepository

--- backend/registrador/src/main/java/br/mackenzie/registrador/repository/CamisaRepository.java
+++ backend/registrador/src/main/java/br/mackenzie/registrador/repository/CamisaRepository.java
@@ -2,9 +2,7 @@ package br.mackenzie.registrador.repository;
 
 import br.mackenzie.registrador.entities.Camisa;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CamisaRepository extends JpaRepository<Camisa, Long> {
 
 }
